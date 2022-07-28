7777aafea158f4fe2bb050ea44600df2974d4ee3
fparadas <felipegparadas@gmail.com>
2022-07-28 16:26:53 -0300

    started with basic data structures

---
+++ src/jobs.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <termios.h>
+
+
+/* Data Structures based on GNU implementation of a shell */
+
+/* A process is a single process.  */
+typedef struct process 
+{
+  struct process *next;       /* next process in pipeline */
+  char **argv;                /* for exec */
+  pid_t pid;                  /* process ID */
+  char completed;             /* true if process has completed */
+  char stopped;               /* true if process has stopped */
+  int status;                 /* reported status value */
+} process;
+
+/* A job is a pipeline of processes.  */
+typedef struct job 
+{
+  struct job *next;           /* next active job */
+  char *command;              /* command line, used for messages */
+  process *first_process;     /* list of processes in this job */
+  pid_t pgid;                 /* process group ID */
+  char notified;              /* true if user told about stopped job */
+  struct termios tmodes;      /* saved terminal modes */
+  int stdin, stdout, stderr;  /* standard i/o channels */
+} job;
+
+
+job * find_job (pid_t pgid);
+
+int job_is_stopped (job *j);
+
+int job_is_completed (job *j);
